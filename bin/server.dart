5bf0ed4d2728bc1c1aae40397b59aee2ce5454cf
Alessandro Maifredi <a.maifredi@campus.unimib.it>
2023-11-07 10:45:13 +0100

    trying to fix connection to db (hope)

--- bin/server.dart
+++ bin/server.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 
-import 'package:mongo_dart/mongo_dart.dart';
+import 'package:dart_ping/dart_ping.dart';
 import 'package:restful_dart/db_driver/db_driver.dart';
-import 'package:restful_dart/player/player_api.dart';
 import 'package:restful_dart/root/root_api.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart';
@@ -10,18 +9,21 @@ import 'package:shelf/shelf_io.dart';
 /// The main entry point for the application
 void main(List<String> args) async {
   // Use any available host or container IP (usually `0.0.0.0`).
-  final ip = InternetAddress.anyIPv4;
-  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final ip = InternetAddress.loopbackIPv4;
+  final serverPort = int.parse(Platform.environment['PORT'] ?? '8080');
   final app = RootApi().router;
   final handler = Pipeline().addMiddleware(logRequests()).addHandler(app);
 
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
-
-  await DbDriver.instance.mongoDb.open();
+  final server = await serve(handler, ip, serverPort);
+  print('Server listening on $ip on port ${server.port}');
+  final ping = Ping('$host:$port', count: 1000, interval: 2);
+  ping.stream.listen((event) {
+    print("New ping: $event");
+  });
+  /*await DbDriver.instance.mongoDb.open();
 
   assert(DbDriver.instance.mongoDb.state == State.open);
   print("Using DB: ${DbDriver.instance.mongoDb.databaseName}");
 
-  app.mount(PlayerApi.uriPath, PlayerApi().router);
+  app.mount(PlayerApi.uriPath, PlayerApi().router);*/
 }
--- lib/db_driver/db_driver.dart
+++ lib/db_driver/db_driver.dart
@@ -1,23 +1,22 @@
-import 'dart:io' show Platform;
+import 'dart:io' show InternetAddress, Platform;
 
 import 'package:mongo_dart/mongo_dart.dart';
+
+/// The host for the database connection
+/// Use the environment variable MONGO_DART_DRIVER_HOST to override
+/// Defaults to 127.0.0.1
+final String host = Platform.environment['MONGO_DART_DRIVER_HOST'] ??
+    InternetAddress.loopbackIPv4.toString();
+
+/// The port for the database connection
+/// Use the environment variable MONGO_DART_DRIVER_PORT to override
+/// Defaults to 27017
+final String port = Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
 
 /// A singleton class to manage the database connection
 class DbDriver {
   /// The database connection
-  final Db _mongoDb = Db('mongodb://mongodb:27017/restfuldart');
-
-  /// The host for the database connection
-  /// Use the environment variable MONGO_DART_DRIVER_HOST to override
-  /// Defaults to 127.0.0.1
-  static final String _host =
-      Platform.environment['MONGO_DART_DRIVER_HOST'] ?? '127.0.0.1';
-
-  /// The port for the database connection
-  /// Use the environment variable MONGO_DART_DRIVER_PORT to override
-  /// Defaults to 27017
-  static final String _port =
-      Platform.environment['MONGO_DART_DRIVER_PORT'] ?? '27017';
+  final Db _mongoDb = Db('mongodb://$host:$port/restfuldart');
 
   /// The default limit for queries
   /// Use the environment variable API_DEFAULT_QUERIES_LIMIT to override
